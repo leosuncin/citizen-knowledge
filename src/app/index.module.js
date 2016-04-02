b175d5a6bea774b7c5c8797b196a68d1d49d9956
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2016-04-01 23:27:28 -0600

    Initial commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('citizenKnowledge', ['ngAnimate', 'ngTouch', 'ngSanitize', 'ngAria', 'restangular', 'ui.router', 'mgcrea.ngStrap', 'toastr']);
+
+})();
